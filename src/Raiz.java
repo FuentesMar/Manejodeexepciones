122877c20f9618eebff13b6ec50f5c734cbc2a11
EidtanFuentes <112510183+EidtanFuentes@users.noreply.github.com>
2022-08-31 21:11:22 -0700

    Actividad ManejodeExcepciones

--- src/Raiz.java
+++ src/Raiz.java
@@ -4,16 +4,15 @@ public class Raiz {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Introduzca el numero al que quiera sacar raiz");
-        int numero = sc.nextInt();
-
         try {
+            System.out.println("Introduzca el numero al que quiera sacar raiz");
+            int numero = sc.nextInt();
             Double raiz;
             if (!(numero < 0)) {
                 raiz = Math.sqrt(numero);
                 System.out.println("El valor de la raiz de " + numero + " es: " + raiz);
             } else {
-                throw new NumeroNegativoException("El numero tiene un valor negativo",
+                throw new NumeroNegativoException("El numero tiene un valor negativo, por lo cual no tiene una raiz",
                         new Throwable("Numero: " + numero));
 
             }
